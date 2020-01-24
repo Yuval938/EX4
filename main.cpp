f0b32da92ca76421c80a60b1a7f111dc514c5f54
valdman40 <valdman40@gmail.com>
2020-01-24 18:06:13 +0200

    read port from args

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
-
-#include "Boot.h"
-
-using namespace boot;
+#include <iostream>
+#include "Server.h"
+#include "SerialServer.h"
+#include "ClientHandler.h"
+#include "MyTestClientHandler.h"
+#include "Solver.h"
+#include "StringReverser.h"
+#include "MyClientHandler.h"
+#include "MatrixSolver.h"
+#include "Searcher.h"
+#include "Searchable.h"
+#include "BFS.h"
+#include "Matrix.h"
+#include "BestFirstSearch.h"
+#include "AStar.h"
+#include "ParallelServer.h"
+#include "DFS.h"
 
 int main(int argc, char *argv[]) {
-    int port=5404;
+    int port = 5402;
     if(argc > 0) {
-      //  port = atoi(argv[0]);
+        port = atoi(argv[1]); // assuming that the only arg i'll get will be a port number
     }
-    Main m;
-    m.main(port);
+    Server *server = new ParallelServer();
+    ClientHandler *clientTest = new MyClientHandler(new MatrixSolver(new BestFirstSearch<pair<int, int>>()));
+    server->open(port, clientTest);
     return 0;
 }
