0bb6a7ead31ab9b6e44cf96cec0feb981210741c
Yuval938 <incyuval@gmail.com>
2020-01-25 13:47:58 +0200

    via argument

--- main.cpp
+++ main.cpp
@@ -1,28 +1,13 @@
-#include <iostream>
-#include "Server.h"
-#include "SerialServer.h"
-#include "ClientHandler.h"
-#include "MyTestClientHandler.h"
-#include "Solver.h"
-#include "StringReverser.h"
-#include "MyClientHandler.h"
-#include "MatrixSolver.h"
-#include "Searcher.h"
-#include "Searchable.h"
-#include "BFS.h"
-#include "Matrix.h"
-#include "BestFirstSearch.h"
-#include "AStar.h"
-#include "ParallelServer.h"
-#include "DFS.h"
+#include "Boot.h"
+
+using namespace boot;
 
 int main(int argc, char *argv[]) {
-    int port = 5402;
-    if(argc > 0) {
+    int port = 5400;
+    if (argc > 0) {
         port = atoi(argv[1]); // assuming that the only arg i'll get will be a port number
     }
-    Server *server = new ParallelServer();
-    ClientHandler *clientTest = new MyClientHandler(new MatrixSolver(new BestFirstSearch<pair<int, int>>()));
-    server->open(port, clientTest);
+    Main m;
+    m.main(port);
     return 0;
 }
