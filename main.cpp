d13db9f895daa34431843d971cc57cd882e93061
Yuval938 <incyuval@gmail.com>
2020-01-25 13:54:54 +0200

    without args works

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace boot;
 
 int main(int argc, char *argv[]) {
     int port = 5400;
-    if (argc > 0) {
+    if (argc > 1) {
         port = atoi(argv[1]); // assuming that the only arg i'll get will be a port number
     }
     Main m;
