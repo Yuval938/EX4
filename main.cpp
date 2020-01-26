5e8cb20bbb90049799f486ae00d79d6c53408c71
Yuval938 <incyuval@gmail.com>
2020-01-26 22:58:06 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace boot;
 
 int main(int argc, char *argv[]) {
-    int port = 5400;
+    int port = 5600;
     if (argc > 1) {
         port = atoi(argv[1]); // assuming that the only arg i'll get will be a port number
     }
